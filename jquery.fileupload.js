b96fe5425cddfeb550b6f66922487c4889b94e64
Samuel Clay <samuel@ofbrooklyn.com>
2011-03-11 13:28:12 -0500

    Fixing issue around Internet Explorer trying to accept drag-and-dropped files (when options.dragDropSupport=true). Checking for files attribute, so Safari and FF still know what to expect.

--- jquery.fileupload.js
+++ jquery.fileupload.js
@@ -7,6 +7,7 @@
  *
  * https://blueimp.net
  * http://www.aquantum.de
+ * GitHub: https://github.com/blueimp/jQuery-File-Upload/wiki
  */
 
 /*jslint browser: true */
@@ -467,10 +468,10 @@
                 return false;
             }
             var dataTransfer = e.originalEvent.dataTransfer;
-            if (dataTransfer) {
+            if (dataTransfer && dataTransfer.files) {
                 dataTransfer.dropEffect = dataTransfer.effectAllowed = 'copy';
+                e.preventDefault();
             }
-            e.preventDefault();
         };
 
         this.onDrop = function (e) {
