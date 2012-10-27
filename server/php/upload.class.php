1e1ec5ab30fd58e4255bd2e01d029b777ae28b80
Sebastian Tschan <sebastian.tschan@gmail.com>
2012-10-27 12:32:29 +0900

    Updated version number.

--- server/php/upload.class.php
+++ server/php/upload.class.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * jQuery File Upload Plugin PHP Class 5.18.1
+ * jQuery File Upload Plugin PHP Class 5.18.2
  * https://github.com/blueimp/jQuery-File-Upload
  *
  * Copyright 2010, Sebastian Tschan
