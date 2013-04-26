c192574f0a918d20a5c0628edc15064119de2ceb
Sebastian Tschan <mail@blueimp.net>
2013-04-26 23:47:30 +0200

    Fixed AngularJS demo settings.

--- js/app.js
+++ js/app.js
@@ -15,17 +15,18 @@
     'use strict';
 
     var isOnGitHub = window.location.hostname === 'blueimp.github.com' ||
-        window.location.hostname === 'blueimp.github.io',
+            window.location.hostname === 'blueimp.github.io',
         url = isOnGitHub ? '//jquery-file-upload.appspot.com/' : 'server/php/';
 
     angular.module('demo', [
         'blueimp.fileupload'
     ])
         .config([
-            'fileUploadProvider',
-            function (fileUploadProvider) {
+            '$httpProvider', 'fileUploadProvider',
+            function ($httpProvider, fileUploadProvider) {
                 if (isOnGitHub) {
                     // Demo settings:
+                    delete $httpProvider.defaults.headers.common['X-Requested-With'];
                     angular.extend(fileUploadProvider.defaults, {
                         disableImageResize: false,
                         maxFileSize: 5000000,
@@ -38,17 +39,22 @@
         .controller('DemoFileUploadController', [
             '$scope', '$http',
             function ($scope, $http) {
-                $scope.loadingFiles = true;
-                $http.get(url)
-                    .then(
-                        function (response) {
-                            $scope.loadingFiles = false;
-                            $scope.queue = response.data.files;
-                        },
-                        function () {
-                            $scope.loadingFiles = false;
-                        }
-                    );
+                if (!isOnGitHub) {
+                    $scope.loadingFiles = true;
+                    $scope.options = {
+                        url: url
+                    };
+                    $http.get(url)
+                        .then(
+                            function (response) {
+                                $scope.loadingFiles = false;
+                                $scope.queue = response.data.files;
+                            },
+                            function () {
+                                $scope.loadingFiles = false;
+                            }
+                        );
+                }
             }
         ])
 
