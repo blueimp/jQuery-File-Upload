ac5adc6742cdb747c03cf8c4fc910a3ae8a5911d
Sebastian Tschan <mail@blueimp.net>
2013-09-06 09:08:25 -0500

    Updated version number.

--- js/jquery.fileupload-angular.js
+++ js/jquery.fileupload-angular.js
@@ -1,5 +1,5 @@
 /*
- * jQuery File Upload AngularJS Plugin 1.4.4
+ * jQuery File Upload AngularJS Plugin 1.4.5
  * https://github.com/blueimp/jQuery-File-Upload
  *
  * Copyright 2013, Sebastian Tschan
