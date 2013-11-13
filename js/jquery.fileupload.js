6c5b705986843d05b580c5854f35cbe4d927c1f3
Sam Tyson <styson@gmail.com>
2013-11-13 12:23:06 -0500

    updated path to jquery.ui.widget

--- js/jquery.fileupload.js
+++ js/jquery.fileupload.js
@@ -18,7 +18,7 @@
         // Register as an anonymous AMD module:
         define([
             'jquery',
-            'jquery.ui.widget'
+            './vendor/jquery.ui.widget'
         ], factory);
     } else {
         // Browser globals:
