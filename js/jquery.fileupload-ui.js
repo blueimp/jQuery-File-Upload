41d2b2d7db714feefb7d939f509de6f88a463630
Sebastian Tschan <mail@blueimp.net>
2014-01-08 13:07:17 -0600

    Updated version number.

--- js/jquery.fileupload-ui.js
+++ js/jquery.fileupload-ui.js
@@ -1,5 +1,5 @@
 /*
- * jQuery File Upload User Interface Plugin 9.4.1
+ * jQuery File Upload User Interface Plugin 9.5.1
  * https://github.com/blueimp/jQuery-File-Upload
  *
  * Copyright 2010, Sebastian Tschan
