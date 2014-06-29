c33b63fe8792cc5308b9d25282bdc1e9c977145f
Sebastian Tschan <mail@blueimp.net>
2014-06-29 20:09:43 +0100

    Updated version number for last commit.

--- server/php/UploadHandler.php
+++ server/php/UploadHandler.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * jQuery File Upload Plugin PHP Class 8.0.1
+ * jQuery File Upload Plugin PHP Class 8.0.2
  * https://github.com/blueimp/jQuery-File-Upload
  *
  * Copyright 2010, Sebastian Tschan
