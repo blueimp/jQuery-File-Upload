620170917557a6eeaf7bfcb9373d3957938fe722
dmitriy.tkachenko <dmitriy.tkachenko@altexsoft>
2014-11-28 15:28:36 +0200

    _initDataAttributes method has been updated to avoid memory leaks in jquery cache

--- js/jquery.fileupload.js
+++ js/jquery.fileupload.js
@@ -1338,10 +1338,15 @@
         _initDataAttributes: function () {
             var that = this,
                 options = this.options,
-                clone = $(this.element[0].cloneNode(false));
+                clone = $(this.element[0].cloneNode(false)),
+                data = clone.data();
+
+            //to avoid memory leaks
+            clone.remove();
+
             // Initialize options set via HTML5 data-attributes:
             $.each(
-                clone.data(),
+                data,
                 function (key, value) {
                     var dataAttributeName = 'data-' +
                         // Convert camelCase to hyphen-ated key:
