7e6191c68d53b040fdd9a60de71462b55e859daf
ToreOlavKristiansen <tore@collabora.no>
2015-10-30 15:38:54 +0100

    Fixed byte conversion bug
    Fixed incorrect conversion from byte to mb to gb.

--- js/jquery.fileupload-ui.js
+++ js/jquery.fileupload-ui.js
@@ -426,13 +426,13 @@
             if (typeof bytes !== 'number') {
                 return '';
             }
-            if (bytes >= 1000000000) {
-                return (bytes / 1000000000).toFixed(2) + ' GB';
+            if (bytes >= 1073741824) {
+                return (bytes / 1073741824).toFixed(2) + ' GB';
             }
-            if (bytes >= 1000000) {
-                return (bytes / 1000000).toFixed(2) + ' MB';
+            if (bytes >= 1048576) {
+                return (bytes / 1048576).toFixed(2) + ' MB';
             }
-            return (bytes / 1000).toFixed(2) + ' KB';
+            return (bytes / 1024).toFixed(2) + ' KB';
         },
 
         _formatBitrate: function (bits) {
