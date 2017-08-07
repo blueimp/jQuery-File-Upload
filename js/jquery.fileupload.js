883241b59da45e0b61a255542dffea9635ee3dfe
cubiclesoft <webmaster@cubiclesoft.com>
2017-08-06 19:49:16 -0700

    Allow maxChunkSize to be a function for dynamic chunk size support.

--- js/jquery.fileupload.js
+++ js/jquery.fileupload.js
@@ -730,7 +730,7 @@
                 promise = dfd.promise(),
                 jqXHR,
                 upload;
-            if (!(this._isXHRUpload(options) && slice && (ub || mcs < fs)) ||
+            if (!(this._isXHRUpload(options) && slice && (ub || ($.type(mcs) === 'function' ? mcs(options) : mcs) < fs)) ||
                     options.data) {
                 return false;
             }
@@ -753,7 +753,7 @@
                 o.blob = slice.call(
                     file,
                     ub,
-                    ub + mcs,
+                    ub + ($.type(mcs) === 'function' ? mcs(o) : mcs),
                     file.type
                 );
                 // Store the current chunk size, as the blob itself
