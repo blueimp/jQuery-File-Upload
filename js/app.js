1f6c8d3593fa0660d7c4a9c10ccc3090ad42380a
Sebastian Tschan <mail@blueimp.net>
2019-07-20 13:04:33 +0900

    Use blueimp+jsdoc+prettier eslint config for JS.
    Use stylelint+pretter for CSS.
    Use package.json files property instead of .npmignore.
    Add a package-lock.json file.

--- js/app.js
+++ js/app.js
@@ -9,93 +9,95 @@
  * https://opensource.org/licenses/MIT
  */
 
-/* jshint nomen:false */
-/* global window, angular */
+/* global angular */
 
-;(function () {
-    'use strict';
+(function() {
+  'use strict';
 
-    var isOnGitHub = window.location.hostname === 'blueimp.github.io',
-        url = isOnGitHub ? '//jquery-file-upload.appspot.com/' : 'server/php/';
+  var isOnGitHub = window.location.hostname === 'blueimp.github.io',
+    url = isOnGitHub ? '//jquery-file-upload.appspot.com/' : 'server/php/';
 
-    angular.module('demo', [
-        'blueimp.fileupload'
+  angular
+    .module('demo', ['blueimp.fileupload'])
+    .config([
+      '$httpProvider',
+      'fileUploadProvider',
+      function($httpProvider, fileUploadProvider) {
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        fileUploadProvider.defaults.redirect = window.location.href.replace(
+          /\/[^/]*$/,
+          '/cors/result.html?%s'
+        );
+        if (isOnGitHub) {
+          // Demo settings:
+          angular.extend(fileUploadProvider.defaults, {
+            // Enable image resizing, except for Android and Opera,
+            // which actually support image resizing, but fail to
+            // send Blob objects via XHR requests:
+            disableImageResize: /Android(?!.*Chrome)|Opera/.test(
+              window.navigator.userAgent
+            ),
+            maxFileSize: 999000,
+            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+          });
+        }
+      }
     ])
-        .config([
-            '$httpProvider', 'fileUploadProvider',
-            function ($httpProvider, fileUploadProvider) {
-                delete $httpProvider.defaults.headers.common['X-Requested-With'];
-                fileUploadProvider.defaults.redirect = window.location.href.replace(
-                    /\/[^\/]*$/,
-                    '/cors/result.html?%s'
-                );
-                if (isOnGitHub) {
-                    // Demo settings:
-                    angular.extend(fileUploadProvider.defaults, {
-                        // Enable image resizing, except for Android and Opera,
-                        // which actually support image resizing, but fail to
-                        // send Blob objects via XHR requests:
-                        disableImageResize: /Android(?!.*Chrome)|Opera/
-                            .test(window.navigator.userAgent),
-                        maxFileSize: 999000,
-                        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
-                    });
-                }
-            }
-        ])
 
-        .controller('DemoFileUploadController', [
-            '$scope', '$http', '$filter', '$window',
-            function ($scope, $http) {
-                $scope.options = {
-                    url: url
-                };
-                if (!isOnGitHub) {
-                    $scope.loadingFiles = true;
-                    $http.get(url)
-                        .then(
-                            function (response) {
-                                $scope.loadingFiles = false;
-                                $scope.queue = response.data.files || [];
-                            },
-                            function () {
-                                $scope.loadingFiles = false;
-                            }
-                        );
-                }
+    .controller('DemoFileUploadController', [
+      '$scope',
+      '$http',
+      '$filter',
+      '$window',
+      function($scope, $http) {
+        $scope.options = {
+          url: url
+        };
+        if (!isOnGitHub) {
+          $scope.loadingFiles = true;
+          $http.get(url).then(
+            function(response) {
+              $scope.loadingFiles = false;
+              $scope.queue = response.data.files || [];
+            },
+            function() {
+              $scope.loadingFiles = false;
             }
-        ])
+          );
+        }
+      }
+    ])
 
-        .controller('FileDestroyController', [
-            '$scope', '$http',
-            function ($scope, $http) {
-                var file = $scope.file,
-                    state;
-                if (file.url) {
-                    file.$state = function () {
-                        return state;
-                    };
-                    file.$destroy = function () {
-                        state = 'pending';
-                        return $http({
-                            url: file.deleteUrl,
-                            method: file.deleteType
-                        }).then(
-                            function () {
-                                state = 'resolved';
-                                $scope.clear(file);
-                            },
-                            function () {
-                                state = 'rejected';
-                            }
-                        );
-                    };
-                } else if (!file.$cancel && !file._index) {
-                    file.$cancel = function () {
-                        $scope.clear(file);
-                    };
-                }
-            }
-        ]);
-
-}());
+    .controller('FileDestroyController', [
+      '$scope',
+      '$http',
+      function($scope, $http) {
+        var file = $scope.file,
+          state;
+        if (file.url) {
+          file.$state = function() {
+            return state;
+          };
+          file.$destroy = function() {
+            state = 'pending';
+            return $http({
+              url: file.deleteUrl,
+              method: file.deleteType
+            }).then(
+              function() {
+                state = 'resolved';
+                $scope.clear(file);
+              },
+              function() {
+                state = 'rejected';
+              }
+            );
+          };
+        } else if (!file.$cancel && !file._index) {
+          file.$cancel = function() {
+            $scope.clear(file);
+          };
+        }
+      }
+    ]);
+})();
