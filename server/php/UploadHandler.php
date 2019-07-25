f042a139a620c65795b62c7c8f186c01f4885903
Gelencsér Gergő <programmerg@gmail.com>
2019-07-25 16:54:42 +0200

    Prevent open_basedir restriction warnings
    You are right! I have updated the condition.

--- server/php/UploadHandler.php
+++ server/php/UploadHandler.php
@@ -324,7 +324,7 @@ class UploadHandler
 
     protected function is_valid_file_object($file_name) {
         $file_path = $this->get_upload_path($file_name);
-        if ($file_name[0] !== '.' && is_file($file_path)) {
+        if (strlen($file_name) > 0 && $file_name[0] !== '.' && is_file($file_path)) {
             return true;
         }
         return false;
@@ -1440,7 +1440,7 @@ class UploadHandler
         $response = array();
         foreach ($file_names as $file_name) {
             $file_path = $this->get_upload_path($file_name);
-            $success = $file_name[0] !== '.' && is_file($file_path) && unlink($file_path);
+            $success = strlen($file_name) > 0 && $file_name[0] !== '.' && is_file($file_path) && unlink($file_path);
             if ($success) {
                 foreach ($this->options['image_versions'] as $version => $options) {
                     if (!empty($version)) {
