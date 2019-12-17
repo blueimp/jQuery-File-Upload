16c74020113652ccb3b367b0532f1e66ab7bb4ef
Rich <rp@richlab.de>
2019-12-17 13:53:19 +0100

    Fix function get_config_bytes
    found error in switch, all cases returning same bytes-val

--- server/php/UploadHandler.php
+++ server/php/UploadHandler.php
@@ -384,9 +384,9 @@ class UploadHandler
         }
         switch ($last) {
             case 'g':
-                $val *= 1024;
+                $val *= 1024 * 1024 * 1024;
             case 'm':
-                $val *= 1024;
+                $val *= 1024 * 1024;
             case 'k':
                 $val *= 1024;
         }
