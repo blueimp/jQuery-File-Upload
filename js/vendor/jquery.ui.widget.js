d5dfbc62f5b06473c0835c43782d6a248f0775dc
Sebastian Tschan <mail@blueimp.net>
2020-02-20 23:37:40 +0900

    Don't swallow errors in `remove` events.
    https://github.com/jquery/jquery-ui/commit/1f2011ece3fe6847874677e9a8210fa202498ccb

--- js/vendor/jquery.ui.widget.js
+++ js/vendor/jquery.ui.widget.js
@@ -48,16 +48,12 @@
     return function( elems ) {
       var events, elem, i;
       for ( i = 0; ( elem = elems[ i ] ) != null; i++ ) {
-        try {
 
-          // Only trigger remove when necessary to save time
-          events = $._data( elem, "events" );
-          if ( events && events.remove ) {
-            $( elem ).triggerHandler( "remove" );
-          }
-
-          // Http://bugs.jquery.com/ticket/8235
-        } catch ( e ) {}
+        // Only trigger remove when necessary to save time
+        events = $._data( elem, "events" );
+        if ( events && events.remove ) {
+          $( elem ).triggerHandler( "remove" );
+        }
       }
       orig( elems );
     };
